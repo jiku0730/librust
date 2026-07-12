99fe1fa6857a68142eb07245f144f06f22cb9572
jiku0730 <github@jiku.jp>
2026-07-13 02:50:26 +0900

    add: greet関数とそのテストを追加。

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn greet(name: &str) -> String {
+    format!("こんにちは、{name}さん！")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,10 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn greet_works() {
+        assert_eq!(greet("たろう"), "こんにちは、たろうさん！");
     }
 }
